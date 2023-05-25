4d487e111fa527479a2e78fc49dba0939228439e
Angelo Bonicelli <a.bonicelli@iungo.it>
2023-05-25 23:22:36 +0200

    work of 23

---
+++ .bash_aliases
@@ -0,0 +1,8 @@
+# My custom aliases
+
+alias git-contribs='git ls-files | while read f; do git blame -w -M -C -C --line-porcelain "$f" | grep -I "^author "; done | sort -f | uniq -ic | sort -n --reverse'
+alias docker-stop-all='docker stop $(docker ps -a -q)'
+alias docker-rm-all='docker rm $(docker ps -a -q)'
+alias git-merged='git branch --merged | egrep -v "(^\*|master|main|dev)" | xargs git branch -d'
+alias apt-upgrade='sudo apt update && sudo apt upgrade && sudo apt autoremove'
+
--- lua/angel/remap.lua
+++ lua/angel/remap.lua
@@ -13,9 +13,18 @@ vim.cmd('set number relativenumber')
 
 -- 4 space tabs
 -- https://stackoverflow.com/questions/1878974/redefine-tab-as-4-spaces
-vim.cmd([[
-    set shiftwidth=4 smarttab
-    set expandtab
-    set tabstop=8 softtabstop=0
-]])
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.smarttab = true
+vim.opt.tabstop = 8
+vim.opt.softtabstop = 0
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
+
+vim.opt.scrolloff = 8
 
