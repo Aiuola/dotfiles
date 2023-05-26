8a51191d91c427f4158becb6e892158c6f8a7335
Angelo Bonicelli <a.bonicelli@iungo.it>
2023-05-26 11:26:23 +0200

    Trying...

--- lua/angel/packer.lua
+++ lua/angel/packer.lua
@@ -77,5 +77,18 @@ return require('packer').startup(function(use)
         use {
             'natebosch/vim-lsc'
         }
+
+        -- Unless you are still migrating, remove the deprecated commands from v1.x
+        vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+        use {
+          "nvim-neo-tree/neo-tree.nvim",
+            branch = "v2.x",
+            requires = {
+              "nvim-lua/plenary.nvim",
+              "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+              "MunifTanjim/nui.nvim",
+            }
+        }
 end)
 
