8ed89453ddf79a8719e9cc14607e2caaa3624d5f
Angelo Bonicelli <angelo.bonicelli@gmail.com>
2023-09-30 19:09:47 +0200

    updated movement

--- nvim/lua/angel/remap.lua
+++ nvim/lua/angel/remap.lua
@@ -5,8 +5,7 @@ vim.keymap.set('n', '<S-Enter>', 'O<ESC>')
 
 vim.keymap.set('n', '<C-a>', 'gg0vG$')
 
-vim.keymap.set('v', 'M', '$')
-vim.keymap.set('v', 'N', '^')
-vim.keymap.set('n', 'M', '$')
-vim.keymap.set('n', 'N', '^')
-
+vim.keymap.set('v', 'P', '$')
+vim.keymap.set('v', 'O', '^')
+vim.keymap.set('n', 'P', '$')
+vim.keymap.set('n', 'O', '^')
