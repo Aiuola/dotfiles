20f9f71d6a1f11636873e2d50daf4ae04cf20fbd
Angelo Bonicelli <a.bonicelli@iungo.it>
2024-09-13 16:58:26 +0200

    updated gitconfig and init script

--- init.sh
+++ init.sh
@@ -2,9 +2,9 @@
 
 conf="configs/"
 
-# This shouldn't change often...
-#rm ~/.gitconfig
-#ln "conf".gitconfig ~/.gitconfig
+rm ~/.gitconfig
+ln "conf".gitconfig ~/.gitconfig
+git config --global user.email = "$1"
 
 rm ~/.config/alacritty/alacritty.yml
 ln "$conf"alacritty.yml ~/.config/alacritty/alacritty.yml
@@ -12,16 +12,16 @@ ln "$conf"alacritty.yml ~/.config/alacritty/alacritty.yml
 rm ~/.ideavimrc
 ln "$conf".ideavimrc ~/.ideavimrc
 
-rm ~/.bash_aliases
-ln "$conf".bash_aliases ~/.bash_aliases
+rm ~/.shell_aliases
+ln "$conf".shell_aliases ~/.shell_aliases
 
-if [ "$1" == "-f" ]; then
-    if [ "$2" == "nvim" ]; then
+if [ "$2" == "-f" ]; then
+    if [ "$3" == "nvim" ]; then
       rm -rf ~/.config/nvim
       cd ~/.config/
       ln -s ~/dotfiles/nvim nvim
     else
-      if [ "$2" == "chad" ]; then
+      if [ "$3" == "chad" ]; then
         rm -rf ~/.config/nvim/lua/custom
         cd ~/.config/nvim/lua/
         ln -s ~/dotfiles/custom custom
@@ -30,15 +30,14 @@ if [ "$1" == "-f" ]; then
         echo "arg 2 was not nvim or chad"
       fi
     fi
-
     if [ $? -eq 0 ]; then
-        echo "Replaced vim direcotry with current config"
+        echo "Replaced vim directory with current config"
     else
         echo "Error while trying to delete vim directory"
         exit 1
     fi
 else
-    echo "First arg was not -f, call with -f for vim config deletion"
+    echo "First arg was not -f, provide -f as the first arg for vim config deletion"
 fi
 
 echo "Configs setup ended"
